abcd6460f7577e85c569278a556339b0cf8f1313
Reviveheadandbodycare <revivenorwood@gmail.com>
2025-06-27 14:57:29 -0400

    revive

---
+++ revive-website/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-800 mb-4">Revive Head & Body Care</h1>
+      <p className="text-lg text-gray-600">Welcome to your sanctuary of relaxation and renewal.</p>
+    </div>
+  );
+}
+
+export default Home;
